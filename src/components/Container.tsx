8411a012653acec0338962c25ff53d4c8bb0f395
Futhememe <kyoro_souza@hotmail.com>
2022-08-27 12:12:44 -0300

    💄 new background color

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,20 +1,20 @@
-import { Flex, useColorMode, FlexProps } from '@chakra-ui/react'
+import { Flex, useColorMode, FlexProps } from "@chakra-ui/react";
 
 export const Container = (props: FlexProps) => {
-  const { colorMode } = useColorMode()
+  const { colorMode } = useColorMode();
 
-  const bgColor = { light: 'white', dark: 'gray.900' }
+  const bgColor = { light: "#191919", dark: "gray.900" };
 
-  const color = { light: 'black', dark: 'white' }
+  const color = { light: "white", dark: "white" };
   return (
     <Flex
       direction="column"
       alignItems="center"
-      minHeight={'100vh'}
+      minHeight={"100vh"}
       justifyContent="center"
       bg={bgColor[colorMode]}
       color={color[colorMode]}
       {...props}
     />
-  )
-}
+  );
+};
