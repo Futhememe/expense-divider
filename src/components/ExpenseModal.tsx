8f3b472f77ed5f6b3ae3ccb56e82568d52e9fc1a
Futhememe <kyoro_souza@hotmail.com>
2022-08-27 13:10:48 -0300

    ✨ expense modal

---
+++ src/components/ExpenseModal.tsx
@@ -0,0 +1,106 @@
+import {
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  InputGroup,
+  InputLeftElement,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  ModalProps,
+  Stack,
+} from "@chakra-ui/react";
+import { useForm, Controller } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+
+export const ExpenseModal = ({ ...rest }: Omit<ModalProps, "children">) => {
+  const schema = yup
+    .object({
+      name: yup.string().required(),
+      amount: yup.number().required(),
+    })
+    .required();
+
+  const {
+    handleSubmit,
+    control,
+    formState: { errors },
+    reset,
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  const onSubmit = (data: any) => console.log(data);
+
+  return (
+    <Modal {...rest}>
+      <ModalOverlay />
+      <ModalContent>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <ModalHeader>Nova Despesa</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <Stack spacing={4}>
+              <FormControl isInvalid={!!errors.name}>
+                <Controller
+                  name="name"
+                  control={control}
+                  rules={{ required: true }}
+                  render={({ field }) => (
+                    <Input {...field} placeholder="Insira o nome da despesa" />
+                  )}
+                />
+              </FormControl>
+
+              <FormControl isInvalid={!!errors.amount}>
+                <Controller
+                  name="amount"
+                  control={control}
+                  rules={{ required: true }}
+                  render={({ field }) => (
+                    <InputGroup>
+                      <InputLeftElement
+                        pointerEvents="none"
+                        color="gray.300"
+                        fontSize="1.2em"
+                      >
+                        $
+                      </InputLeftElement>
+                      <Input
+                        {...field}
+                        type="number"
+                        placeholder="Insira o valor"
+                      />
+                    </InputGroup>
+                  )}
+                />
+              </FormControl>
+            </Stack>
+          </ModalBody>
+          <ModalFooter>
+            <Button
+              variant="ghost"
+              mr={3}
+              type="button"
+              onClick={() => {
+                reset();
+                rest.onClose();
+              }}
+            >
+              Cancelar
+            </Button>
+            <Button colorScheme="green" type="submit">
+              Adicionar
+            </Button>
+          </ModalFooter>
+        </form>
+      </ModalContent>
+    </Modal>
+  );
+};
