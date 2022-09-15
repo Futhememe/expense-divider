ca021755e2b20bf51a46c7eb387f7aaf2a9e24ba
Futhememe <kyoro_souza@hotmail.com>
2022-09-14 21:16:35 -0300

    🚧 new icon to edit percentage

--- src/components/Percentage.tsx
+++ src/components/Percentage.tsx
@@ -1,9 +1,28 @@
-import { Flex, Text } from "@chakra-ui/react";
+import { EditIcon } from "@chakra-ui/icons";
+import { Flex, IconButton, Text, Tooltip } from "@chakra-ui/react";
 
 export const Percentage = () => {
   return (
-    <Flex w="100%" bgColor="#252525" p="1rem">
-      <Text fontWeight="bold">🚧 Esther -&gt; 25% | Gustavo -&gt; 75%</Text>
-    </Flex>
+    <Tooltip
+      label="Clique no icone ao lado para editar as porcentagens e nomes"
+      placement="bottom-start"
+    >
+      <Flex
+        w="100%"
+        bgColor="#252525"
+        py="0.25rem"
+        px="1rem"
+        alignItems="center"
+        justifyContent="space-between"
+      >
+        <Text fontWeight="bold">🚧 Esther -&gt; 25% | Gustavo -&gt; 75%</Text>
+        <IconButton
+          colorScheme="transparent"
+          aria-label="edit expense"
+          icon={<EditIcon />}
+          onClick={() => {}}
+        />
+      </Flex>
+    </Tooltip>
   );
 };
