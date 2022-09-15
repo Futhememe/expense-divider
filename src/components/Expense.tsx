0a293703f863c96e435ff54ff3a4cc675a443f32
Futhememe <kyoro_souza@hotmail.com>
2022-09-14 21:32:52 -0300

    🎨 new expense component

---
+++ src/components/Expense.tsx
@@ -0,0 +1,76 @@
+import { DeleteIcon, EditIcon } from "@chakra-ui/icons";
+import {
+  Divider,
+  Flex,
+  Heading,
+  Highlight,
+  IconButton,
+  ListItem,
+  UnorderedList,
+} from "@chakra-ui/react";
+
+import { formatToBRL } from "../utils/formatters";
+
+interface IExpense {
+  name: string;
+  total: number;
+  firstAmount: number;
+  secondAmount: number;
+  onClickEdit: () => void;
+  onClickDelete: () => void;
+}
+
+export const Expense = ({
+  name,
+  total,
+  firstAmount,
+  secondAmount,
+  onClickEdit,
+  onClickDelete,
+}: IExpense) => {
+  return (
+    <Flex w="100%" flexDir="column">
+      <Flex justifyContent="space-between">
+        <Heading fontSize="2xl" mb="1rem">
+          {name} = &nbsp;
+          <Highlight query={formatToBRL(total)} styles={{ color: "#C39A47" }}>
+            {formatToBRL(total)}
+          </Highlight>
+        </Heading>
+        <Flex>
+          <IconButton
+            colorScheme="transparent"
+            aria-label="edit expense"
+            icon={<EditIcon />}
+            onClick={onClickEdit}
+          />
+          <IconButton
+            colorScheme="transparent"
+            aria-label="delete expense"
+            onClick={onClickDelete}
+            icon={<DeleteIcon />}
+          />
+        </Flex>
+      </Flex>
+      <UnorderedList fontWeight="medium" mb="1.5rem">
+        <ListItem mb="0.5rem">
+          <Highlight
+            query={formatToBRL(firstAmount)}
+            styles={{ color: "#59936D" }}
+          >
+            {`Esther -> ${formatToBRL(firstAmount)}`}
+          </Highlight>
+        </ListItem>
+        <ListItem>
+          <Highlight
+            query={formatToBRL(secondAmount)}
+            styles={{ color: "#59936D" }}
+          >
+            {`Gustavo -> ${formatToBRL(secondAmount)}`}
+          </Highlight>
+        </ListItem>
+      </UnorderedList>
+      <Divider />
+    </Flex>
+  );
+};
